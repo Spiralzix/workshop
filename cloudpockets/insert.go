227004ef15ae97adad5b9283c66f9cb3a17f1d92
lnwsitgod <sittichai.snsap@gmail.com>
2023-01-21 14:41:23 +0700

    update CreateCloudPoacket - update response error with echo NewHTTPError

--- cloudpockets/insert.go
+++ cloudpockets/insert.go
@@ -10,13 +10,13 @@ func (h *handler) CreateCloudPoacket(c echo.Context) error {
 	e := CloudPocket{}
 	err := c.Bind(&e)
 	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: "Cannot bind request"})
+		return echo.NewHTTPError(http.StatusBadRequest, "Cannot bind request")
 	}
 
-	row := h.db.QueryRow("INSERT INTO cloud_pockets (Id, Name, Catagory, Currency, Balance) values ($1, $2, $3, $4, $5)  RETURNING id", e.Id, e.Name, e.Category, e.Currency, e.Balance)
-	err = row.Scan(&e.Id)
+	row := h.db.QueryRow("INSERT INTO cloud_pockets (Id, Name, Catagory, Currency, Balance) values ($1, $2, $3, $4, $5)  RETURNING id", e.ID, e.Name, e.Category, e.Currency, e.Balance)
+	err = row.Scan(&e.ID)
 	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusCreated, e)
 }
