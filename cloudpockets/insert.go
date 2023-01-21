54a895b975e332b04a6e674a6c8b1fa4a767e0de
phonlakid <lstartos4@gmail.com>
2023-01-22 05:09:51 +0700

    Update insert.go
    แก้ไข Update insert.go  ให้เหมือน account.go

--- cloudpockets/insert.go
+++ cloudpockets/insert.go
@@ -3,20 +3,39 @@ package cloudpockets
 import (
 	"net/http"
 
+	"github.com/kkgo-software-engineering/workshop/mlog"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 )
 
-func (h *handler) CreateCloudPoacket(c echo.Context) error {
+const (
+	cStmt         = "INSERT INTO cloud_pockets (Id, Name, Catagory, Currency, Balance) values ($1, $2, $3, $4, $5)  RETURNING id;"
+	cBalanceLimit = 10000
+)
+
+var (
+	hErrBalanceLimitExceed = echo.NewHTTPError(http.StatusBadRequest,
+		"create account balance exceed limitation")
+)
+
+func (h handler) CreateCloudPoacket(c echo.Context) error {
+	logger := mlog.L(c)
+	ctx := c.Request().Context()
 	e := CloudPocket{}
 	err := c.Bind(&e)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, "Cannot bind request")
+		logger.Error("bad request body", zap.Error(err))
+		return echo.NewHTTPError(http.StatusBadRequest, "bad request body", err.Error())
 	}
 
-	row := h.db.QueryRow("INSERT INTO cloud_pockets (Id, Name, Catagory, Currency, Balance) values ($1, $2, $3, $4, $5)  RETURNING id", e.ID, e.Name, e.Category, e.Currency, e.Balance)
-	err = row.Scan(&e.ID)
+	var lastInsertId string
+	err = h.db.QueryRowContext(ctx, cStmt, e.ID, e.Name, e.Category, e.Currency, e.Balance).Scan(&lastInsertId)
 	if err != nil {
-		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		logger.Error("query row error", zap.Error(err))
+		return err
 	}
+
+	logger.Info("create successfully", zap.String("id", lastInsertId))
+	e.ID = lastInsertId
 	return c.JSON(http.StatusCreated, e)
 }
